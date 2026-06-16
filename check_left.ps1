096ce8ad1c63ece2251be67ccd5203faeab6a668
O Rafa da Opus <rafa@opus.com>
2026-06-16 17:00:11 -0300

    chore: remove scratch scripts

--- check_left.ps1
+++
@@ -1,7 +0,0 @@
-Add-Type -AssemblyName System.Drawing
-$bmp = [System.Drawing.Bitmap]::FromFile('images\jaleco-certo-2.png')
-for ($i = 0; $i -lt $bmp.Height; $i += 50) {
-    $c = $bmp.GetPixel(0, $i)
-    Write-Host "Pixel (0, $i): R=$($c.R) G=$($c.G) B=$($c.B)"
-}
-$bmp.Dispose()
--- check_pixels3.ps1
+++
@@ -1,5 +0,0 @@
-Add-Type -AssemblyName System.Drawing
-$bmp = [System.Drawing.Bitmap]::FromFile('images\jaleco-certo-2.png')
-$c = $bmp.GetPixel(10, 10)
-Write-Host "Pixel (10, 10): R=$($c.R) G=$($c.G) B=$($c.B) A=$($c.A)"
-$bmp.Dispose()
